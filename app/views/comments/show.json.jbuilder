c3e3664f28cc3898c7d7a41cf9899a78154474c6
taherhek <taher.hekmatfar@yahoo.com>
2015-02-27 18:15:49 +0000

    Finish static pages

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :name, :email, :content, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
