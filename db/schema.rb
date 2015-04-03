8c354d8b6bf9c6deda89d3309aa29b2ec5cc4426
taherhek <taher.hekmatfar@yahoo.com>
2015-04-03 14:33:20 +0000

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307190619) do
+ActiveRecord::Schema.define(version: 20150403135440) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150307190619) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
